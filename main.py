886ad0894d960d6e8733087cffe3bf9a790b9766
Miha Dolinar <mdolinar@plume.com>
2022-04-23 14:24:28 +0200

    Update on main an requirements

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import argparse
 import yaml
+import requests
+import csv
 from datetime import date
 
+
+def csv_to_array(csv_url):
+    with requests.Session() as s:
+        download = s.get(csv_url)
+        decoded_content = download.content.decode("utf-8")
+        cr = csv.reader(decoded_content.splitlines(), delimiter=",")
+        my_list = list(cr)
+        return my_list
+
+
+def get_csv_url(ad_network, date):
+    if len(ad_network) > 0:
+        for url in ad_network:
+            if (
+                str(date.year) in url
+                and str(date.month) in url
+                and str(date.day) in url
+            ):
+                return url
+
+
 def pars_yaml():
-    with open('advertising_network.yaml', 'r') as stream:
+    with open("advertising_network.yaml", "r") as stream:
         try:
-            ad_network = (yaml.safe_load(stream))
+            ad_network = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
     return ad_network
+
 
 def parse_cli():
     parser = argparse.ArgumentParser(
@@ -21,7 +45,7 @@ def parse_cli():
         action="store",
         required=True,
         type=str,
-        choices=['SuperNetwork', 'AdUmbrella'],
+        choices=["SuperNetwork", "AdUmbrella"],
         help='Choose between "SuperNetwork" or "AdUmbrella"',
     )
     parser.add_argument(
@@ -35,17 +59,23 @@ def parse_cli():
 
     return vars(parser.parse_args())
 
+
 def main():
     """
     Entry point for the program
     """
+    # Parse arguments
     cli_config = parse_cli()
     ad_network = cli_config["adNetwork"]
     date = cli_config["date"]
 
-    print(date)
+    # Pars yaml
     data = pars_yaml()
-    print(data['reports'][ad_network])
+    data_url = data["reports"][ad_network]
+
+    csv_url = get_csv_url(data_url, date)
+    csv_array = csv_to_array(csv_url)
+    print(csv_array)
 
 
 if __name__ == "__main__":
